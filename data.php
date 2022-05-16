68c24df412fd8168c4dde5072c512ed0afb9fac4
Pallavi <68183562+Pallavi-0206@users.noreply.github.com>
2022-05-16 20:31:21 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,10 @@
+<?php 
+
+$fname = filter_input(INPUT_POST,'Full Name');
+$lname = filter_input(INPUT_POST,'Full Name');
+$email = filter_input(INPUT_POST,'Full Name');
+$amount = filter_input(INPUT_POST,'Full Name');
+$message = filter_input(INPUT_POST,'Full Name');
+
+
+?>
